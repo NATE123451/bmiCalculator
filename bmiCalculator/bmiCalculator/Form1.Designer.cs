82b53bcead420e937d7b3a8e11c3709a2992a8d7
Nathan <nate19981@googlemail.com>
2017-12-19 18:53:09 +0000

    Add files via upload

---
+++ bmiCalculator/bmiCalculator/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace bmiCalculator
+{
+    partial class bmi_calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(bmi_calculator));
+            this.title_lbl = new System.Windows.Forms.Label();
+            this.height_lbl = new System.Windows.Forms.Label();
+            this.weight_lbl = new System.Windows.Forms.Label();
+            this.rdo_group = new System.Windows.Forms.GroupBox();
+            this.metric_rdo = new System.Windows.Forms.RadioButton();
+            this.imperial_rdo = new System.Windows.Forms.RadioButton();
+            this.heightMain_txt = new System.Windows.Forms.TextBox();
+            this.heightInch_txt = new System.Windows.Forms.TextBox();
+            this.weightMain_txt = new System.Windows.Forms.TextBox();
+            this.heightEntry1_lbl = new System.Windows.Forms.Label();
+            this.heightEntry2_lbl = new System.Windows.Forms.Label();
+            this.weightEntry_lbl = new System.Windows.Forms.Label();
+            this.calculate_btn = new System.Windows.Forms.Button();
+            this.bmi_lbl = new System.Windows.Forms.Label();
+            this.bmiOutput_txt = new System.Windows.Forms.TextBox();
+            this.heightEntryCM_lbl = new System.Windows.Forms.Label();
+            this.weightEntryKG_lbl = new System.Windows.Forms.Label();
+            this.clear_btn = new System.Windows.Forms.Button();
+            this.rdo_group.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // title_lbl
+            // 
+            this.title_lbl.AutoSize = true;
+            this.title_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title_lbl.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.title_lbl.Location = new System.Drawing.Point(74, 9);
+            this.title_lbl.Name = "title_lbl";
+            this.title_lbl.Size = new System.Drawing.Size(236, 44);
+            this.title_lbl.TabIndex = 0;
+            this.title_lbl.Text = "BMI Calculator";
+            this.title_lbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.title_lbl.UseCompatibleTextRendering = true;
+            // 
+            // height_lbl
+            // 
+            this.height_lbl.AutoSize = true;
+            this.height_lbl.Location = new System.Drawing.Point(40, 96);
+            this.height_lbl.Name = "height_lbl";
+            this.height_lbl.Size = new System.Drawing.Size(66, 13);
+            this.height_lbl.TabIndex = 1;
+            this.height_lbl.Text = "Your Height:";
+            // 
+            // weight_lbl
+            // 
+            this.weight_lbl.AutoSize = true;
+            this.weight_lbl.Location = new System.Drawing.Point(40, 140);
+            this.weight_lbl.Name = "weight_lbl";
+            this.weight_lbl.Size = new System.Drawing.Size(69, 13);
+            this.weight_lbl.TabIndex = 2;
+            this.weight_lbl.Text = "Your Weight:";
+            // 
+            // rdo_group
+            // 
+            this.rdo_group.Controls.Add(this.metric_rdo);
+            this.rdo_group.Controls.Add(this.imperial_rdo);
+            this.rdo_group.Location = new System.Drawing.Point(43, 249);
+            this.rdo_group.Name = "rdo_group";
+            this.rdo_group.Size = new System.Drawing.Size(110, 105);
+            this.rdo_group.TabIndex = 3;
+            this.rdo_group.TabStop = false;
+            this.rdo_group.Text = "Units Selection";
+            // 
+            // metric_rdo
+            // 
+            this.metric_rdo.AutoSize = true;
+            this.metric_rdo.Location = new System.Drawing.Point(7, 72);
+            this.metric_rdo.Name = "metric_rdo";
+            this.metric_rdo.Size = new System.Drawing.Size(54, 17);
+            this.metric_rdo.TabIndex = 1;
+            this.metric_rdo.Text = "Metric";
+            this.metric_rdo.UseVisualStyleBackColor = true;
+            this.metric_rdo.CheckedChanged += new System.EventHandler(this.metric_rdo_CheckedChanged);
+            // 
+            // imperial_rdo
+            // 
+            this.imperial_rdo.AutoSize = true;
+            this.imperial_rdo.Location = new System.Drawing.Point(7, 32);
+            this.imperial_rdo.Name = "imperial_rdo";
+            this.imperial_rdo.Size = new System.Drawing.Size(61, 17);
+            this.imperial_rdo.TabIndex = 0;
+            this.imperial_rdo.Text = "Imperial";
+            this.imperial_rdo.UseVisualStyleBackColor = true;
+            this.imperial_rdo.CheckedChanged += new System.EventHandler(this.imperial_rdo_CheckedChanged);
+            // 
+            // heightMain_txt
+            // 
+            this.heightMain_txt.Location = new System.Drawing.Point(126, 93);
+            this.heightMain_txt.Name = "heightMain_txt";
+            this.heightMain_txt.Size = new System.Drawing.Size(100, 20);
+            this.heightMain_txt.TabIndex = 4;
+            // 
+            // heightInch_txt
+            // 
+            this.heightInch_txt.Location = new System.Drawing.Point(254, 93);
+            this.heightInch_txt.Name = "heightInch_txt";
+            this.heightInch_txt.Size = new System.Drawing.Size(100, 20);
+            this.heightInch_txt.TabIndex = 5;
+            // 
+            // weightMain_txt
+            // 
+            this.weightMain_txt.Location = new System.Drawing.Point(126, 137);
+            this.weightMain_txt.Name = "weightMain_txt";
+            this.weightMain_txt.Size = new System.Drawing.Size(100, 20);
+            this.weightMain_txt.TabIndex = 6;
+            // 
+            // heightEntry1_lbl
+            // 
+            this.heightEntry1_lbl.AutoSize = true;
+            this.heightEntry1_lbl.Location = new System.Drawing.Point(156, 116);
+            this.heightEntry1_lbl.Name = "heightEntry1_lbl";
+            this.heightEntry1_lbl.Size = new System.Drawing.Size(0, 13);
+            this.heightEntry1_lbl.TabIndex = 7;
+            // 
+            // heightEntry2_lbl
+            // 
+            this.heightEntry2_lbl.AutoSize = true;
+            this.heightEntry2_lbl.Location = new System.Drawing.Point(278, 116);
+            this.heightEntry2_lbl.Name = "heightEntry2_lbl";
+            this.heightEntry2_lbl.Size = new System.Drawing.Size(0, 13);
+            this.heightEntry2_lbl.TabIndex = 8;
+            // 
+            // weightEntry_lbl
+            // 
+            this.weightEntry_lbl.AutoSize = true;
+            this.weightEntry_lbl.Location = new System.Drawing.Point(140, 160);
+            this.weightEntry_lbl.Name = "weightEntry_lbl";
+            this.weightEntry_lbl.Size = new System.Drawing.Size(0, 13);
+            this.weightEntry_lbl.TabIndex = 9;
+            // 
+            // calculate_btn
+            // 
+            this.calculate_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculate_btn.Location = new System.Drawing.Point(201, 249);
+            this.calculate_btn.Name = "calculate_btn";
+            this.calculate_btn.Size = new System.Drawing.Size(153, 49);
+            this.calculate_btn.TabIndex = 10;
+            this.calculate_btn.Text = "Calculate BMI";
+            this.calculate_btn.UseVisualStyleBackColor = true;
+            this.calculate_btn.Click += new System.EventHandler(this.calculate_btn_Click);
+            // 
+            // bmi_lbl
+            // 
+            this.bmi_lbl.AutoSize = true;
+            this.bmi_lbl.Location = new System.Drawing.Point(40, 196);
+            this.bmi_lbl.Name = "bmi_lbl";
+            this.bmi_lbl.Size = new System.Drawing.Size(54, 13);
+            this.bmi_lbl.TabIndex = 11;
+            this.bmi_lbl.Text = "Your BMI:";
+            // 
+            // bmiOutput_txt
+            // 
+            this.bmiOutput_txt.Location = new System.Drawing.Point(126, 193);
+            this.bmiOutput_txt.Name = "bmiOutput_txt";
+            this.bmiOutput_txt.ReadOnly = true;
+            this.bmiOutput_txt.Size = new System.Drawing.Size(228, 20);
+            this.bmiOutput_txt.TabIndex = 12;
+            // 
+            // heightEntryCM_lbl
+            // 
+            this.heightEntryCM_lbl.AutoSize = true;
+            this.heightEntryCM_lbl.Location = new System.Drawing.Point(140, 116);
+            this.heightEntryCM_lbl.Name = "heightEntryCM_lbl";
+            this.heightEntryCM_lbl.Size = new System.Drawing.Size(0, 13);
+            this.heightEntryCM_lbl.TabIndex = 13;
+            this.heightEntryCM_lbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // weightEntryKG_lbl
+            // 
+            this.weightEntryKG_lbl.AutoSize = true;
+            this.weightEntryKG_lbl.Location = new System.Drawing.Point(138, 160);
+            this.weightEntryKG_lbl.Name = "weightEntryKG_lbl";
+            this.weightEntryKG_lbl.Size = new System.Drawing.Size(0, 13);
+            this.weightEntryKG_lbl.TabIndex = 14;
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear_btn.Location = new System.Drawing.Point(201, 305);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(153, 49);
+            this.clear_btn.TabIndex = 15;
+            this.clear_btn.Text = "Clear";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // bmi_calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 389);
+            this.Controls.Add(this.clear_btn);
+            this.Controls.Add(this.weightEntryKG_lbl);
+            this.Controls.Add(this.heightEntryCM_lbl);
+            this.Controls.Add(this.bmiOutput_txt);
+            this.Controls.Add(this.bmi_lbl);
+            this.Controls.Add(this.calculate_btn);
+            this.Controls.Add(this.weightEntry_lbl);
+            this.Controls.Add(this.heightEntry2_lbl);
+            this.Controls.Add(this.heightEntry1_lbl);
+            this.Controls.Add(this.weightMain_txt);
+            this.Controls.Add(this.heightInch_txt);
+            this.Controls.Add(this.heightMain_txt);
+            this.Controls.Add(this.rdo_group);
+            this.Controls.Add(this.weight_lbl);
+            this.Controls.Add(this.height_lbl);
+            this.Controls.Add(this.title_lbl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "bmi_calculator";
+            this.Text = "BMI Calculator";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.rdo_group.ResumeLayout(false);
+            this.rdo_group.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title_lbl;
+        private System.Windows.Forms.Label height_lbl;
+        private System.Windows.Forms.Label weight_lbl;
+        private System.Windows.Forms.GroupBox rdo_group;
+        private System.Windows.Forms.RadioButton metric_rdo;
+        private System.Windows.Forms.RadioButton imperial_rdo;
+        private System.Windows.Forms.TextBox heightMain_txt;
+        private System.Windows.Forms.TextBox heightInch_txt;
+        private System.Windows.Forms.TextBox weightMain_txt;
+        private System.Windows.Forms.Label heightEntry1_lbl;
+        private System.Windows.Forms.Label heightEntry2_lbl;
+        private System.Windows.Forms.Label weightEntry_lbl;
+        private System.Windows.Forms.Button calculate_btn;
+        private System.Windows.Forms.Label bmi_lbl;
+        private System.Windows.Forms.TextBox bmiOutput_txt;
+        private System.Windows.Forms.Label heightEntryCM_lbl;
+        private System.Windows.Forms.Label weightEntryKG_lbl;
+        private System.Windows.Forms.Button clear_btn;
+    }
+}
+
---
+++ bmiCalculator/bmiCalculator/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bmiCalculator
+{
+    public partial class bmi_calculator : Form
+    {
+        //Global variable used to tell the unit used for data entry
+        int unitType = 0;
+
+        public bmi_calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clear_heightWeightLbl();
+        }
+
+        /*
+         * Method for clearing the fields of text to be reused, either for new data or a change of units.
+         */
+        public void clear_heightWeightLbl()
+        {
+            heightEntry1_lbl.Text = "";
+            heightEntry2_lbl.Text = "";
+            weightEntry_lbl.Text = "";
+            heightEntryCM_lbl.Text = "";
+            weightEntryKG_lbl.Text = "";
+            bmiOutput_txt.Text = "";
+        }
+
+        /*
+         * Method for setting the fields of text to imperial units, runs after the radio button is selected.
+         */
+        private void imperial_rdo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (imperial_rdo.Checked)
+            {
+                //Sets up unitType int to the imperial setting for later use in a switch statement.
+                unitType = 1;
+                clear_heightWeightLbl();
+                //Makes sure the entry field for inches is visible to the user if they are working with imperial units.
+                heightInch_txt.Visible = true;
+
+                //Sets up the titles for imperial units and hides the metric titles.
+                heightEntry1_lbl.Text = "(Feet)";
+                heightEntry2_lbl.Text = "(Inches)";
+                heightEntryCM_lbl.Text = "";
+                weightEntry_lbl.Text = "(Pounds[lbs])";
+                weightEntryKG_lbl.Text = "";
+            }
+        }
+
+        /*
+         * Method for setting the fields of text to metric units, runs after the radio button is selected.
+         */
+        private void metric_rdo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (metric_rdo.Checked)
+            {
+                //Sets up unitType int to the metric setting for later use in a switch statement.
+                unitType = 2;
+                clear_heightWeightLbl();
+                //Makes sure the entry field for inches is hidden to the user if they are working with metric units.
+                heightInch_txt.Visible = false;
+
+                //Sets up the titles for metric units and hides the imperial titles.
+                heightEntry1_lbl.Text = "";
+                heightEntry2_lbl.Text = "";
+                heightEntryCM_lbl.Text = "(Centimeters)";
+                weightEntry_lbl.Text = "";
+                weightEntryKG_lbl.Text = "(Kilograms[Kg])";
+            }
+        }
+
+        /*
+         * Method containing the main program code. Validates user entry to ensure that a unit type has been selected and the appropriate data has been
+         * entered by the user before continuing. Contains a case statement that will calculate the BMI with the imperial or metric formula depending 
+         * on the users selection, this is determined with the unitType variable setup earlier in the program.
+         */
+        private void calculate_btn_Click(object sender, EventArgs e)
+        {
+            double totalHeightInch = 0;
+            double userBMI = 0;
+            string weightCatagory = "";
+
+            //If statement to validate the users entry data before continuing with the program. Checks that a unit system has been selected and that the 
+            //appropriate height and weight data has been entered.
+            if (unitType < 1 || unitType > 2) {
+                MessageBox.Show("Please select the appropriate units for your entry data.");
+                return;
+            }
+            else if (unitType == 1 && (heightMain_txt.Text == "" || heightInch_txt.Text == "" || weightMain_txt.Text == ""))
+            {
+                MessageBox.Show("Please enter your height and weight in the appropriate spaces.");
+                return;
+            }
+            else if (unitType == 2 && (heightMain_txt.Text == "" || weightMain_txt.Text == ""))
+            {
+                MessageBox.Show("Please enter your height and weight in the appropriate spaces.");
+                return;
+            }
+            
+            double userHeight = double.Parse(heightMain_txt.Text);
+            double userWeight = double.Parse(weightMain_txt.Text);
+            int userHeightInches = 0;
+
+            //Only takes in the users inches measurement if the imperial unit option was selected.
+            if (unitType == 1) {
+                userHeightInches = int.Parse(heightInch_txt.Text);
+            }
+
+            //Switch statement that contains the code to calculate the users bmi and output that value along with the appropriate bmi catagory.
+            switch (unitType)
+            {
+                //Finds the users total height in inches, calculates the users bmi using the imperial formula. Formats the output string to show the bmi and
+                //the appropriate bmi catagory. Displays the final bmi and the catagory to the user.
+                case 1:
+                    totalHeightInch = Convert.ToDouble(imperialTotalHeight(userHeight, userHeightInches));
+                    userBMI = calcImperialBMI(userWeight, totalHeightInch);
+                    string tempString1 = bmiCatagory(userBMI, weightCatagory);
+                    bmiOutput_txt.Text = (Convert.ToString(userBMI)) + tempString1;
+                    break;
+                //Calculates the users bmi using the metric formula. Formats the output string to show the bmi and the appropriate bmi catagory.
+                //Displays the final bmi and the catagory to the user.
+                case 2:
+                    userBMI = calcMetricBMI(userWeight, userHeight);
+                    string tempString2 = bmiCatagory(userBMI, weightCatagory);
+                    bmiOutput_txt.Text = (Convert.ToString(userBMI)) + tempString2;
+                    break;
+            }
+        }
+
+        /*
+         * Method to find the users height in inches to be used in the bmi calculation. 
+         * Converts feet to inches and adds any remaining inches to the total, then returns this value.
+         */
+        public int imperialTotalHeight(double userHeight, int userHeightInches)
+        {
+            int totalInches = 0;
+
+            totalInches += Convert.ToInt32(userHeight * 12);
+            totalInches += userHeightInches;
+
+            return totalInches;
+        }
+
+        /*
+         * Method to calculate the users bmi using the imperial formula. Rounds the output to 2 decimal places and returns the value.
+         */
+        public double calcImperialBMI(double userWeight, double totalHeightInch) {
+            double tempWeightCalc;
+            double heightSquared = 0;
+            double tempBMI = 0;
+
+            tempWeightCalc = (userWeight * 703);
+            heightSquared = (totalHeightInch * totalHeightInch);
+            tempBMI = (tempWeightCalc / heightSquared);
+
+            tempBMI = Math.Round(tempBMI, 2);
+
+            return tempBMI;
+        }
+
+        /*
+         * Method to calculate the users bmi using the metric formula. Rounds the output to 2 decimal places and returns the value.
+         */
+        public double calcMetricBMI(double userWeight, double userHeight) {
+            double tempBMI = 0;
+
+            tempBMI = ((userWeight / userHeight / userHeight) * 10000);
+
+            tempBMI = Math.Round(tempBMI, 2);
+
+            return tempBMI;
+        }
+
+        /*
+         * Method to find the appropriate bmi catagory for the calculated bmi. Sets up the catagory section of the output string. 
+         * Returns a string value containing this section to be concatenated to create the final output string.
+         */
+        public string bmiCatagory(double userBMI, string weightCatagory) {
+            double underWeight = 18.50;
+            double normalWeight = 24.99;
+            double overWeight = 29.99;
+
+            if (userBMI < underWeight)
+            {
+                weightCatagory = ". You are under weight.";
+                return weightCatagory;
+            }
+            else if (userBMI >= underWeight && userBMI <= normalWeight)
+            {
+                weightCatagory = ". You are a normal weight.";
+                return weightCatagory;
+            }
+            else if (userBMI > normalWeight && userBMI <= overWeight)
+            {
+                weightCatagory = ". You are over weight.";
+                return weightCatagory;
+            }
+            else {
+                weightCatagory = ". You are obese.";
+                return weightCatagory;
+            }
+        }
+
+        /*
+         *Method to clear the form of the users input data and unit selection for re-use. Also clears the system generated titles from the form.
+         */
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            clear_heightWeightLbl();
+
+            heightMain_txt.Text = "";
+            heightInch_txt.Text = "";
+            weightMain_txt.Text = "";
+
+            imperial_rdo.Checked = false;
+            metric_rdo.Checked = false;
+        }
+    }
+}
